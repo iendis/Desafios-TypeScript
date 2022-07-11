2565d430d939b0b8bea6b4c330ec9e3e67faefe9
Iendis <iendisforjaz@gmail.com>
2022-07-11 16:10:41 -0300

    commit desafio-dio

---
+++ desafio1.ts
@@ -0,0 +1,16 @@
+interface employe {
+    code: number
+    name: string
+}
+const employee: employe = {
+    code: 10,
+    name: "John"
+}
+
+employee.code = 10;
+employee.name = "John";
+
+
+
+
+console.log()
---
+++ desafio2.ts
@@ -0,0 +1,29 @@
+interface Pessoa{
+    nome: String,
+    idade: number,
+    profissao: "Atriz" | "Padeiro"
+}
+
+const pessoa1: Pessoa = {
+    nome: "Maria",
+    idade: 29,
+    profissao: "Atriz"
+}
+
+const pessoa2: Pessoa = {
+    nome: "Roberto",
+    idade: 19,
+    profissao: "Padeiro"
+}
+
+const pessoa3: Pessoa = {
+    nome: "Laura",
+    idade: 32,
+    profissao: "Atriz"
+}
+
+const pessoa4: Pessoa = {
+    nome: "Carlos",
+    idade: 19,
+    profissao: "Padeiro"
+}
